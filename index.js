39b8856bb80b2faf2c4964ac72207c47b6503cf8
Prawar_19 <prawar@eximiusvc.com>
2023-01-04 23:13:42 +0530

    small changes

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart')
 const orderRoute = require('./routes/order')
-
+const stripeRoute = require('./routes/stripe')
+const cors = require("cors");
 dotenv.config();
 
 mongoose
@@ -16,12 +17,14 @@ mongoose
     .catch((err) => {
         console.log(err)
     });
+app.use(cors());
 app.use(express.json());
 app.use('/api/auth',authRoute)
 app.use('/api/users',userRoute)
 app.use('/api/products',productRoute);
 app.use('/api/cart',cartRoute);
 app.use('/api/orders',orderRoute);
+app.use('/api/checkout',stripeRoute)
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('backend server up')
